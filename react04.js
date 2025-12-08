672a6893814bd4bd9300d7b74c34c8037b2b55c5
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2025-12-08 14:16:29 +0530

    Create react04.js

---
+++ react04.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";\
+class MyApp extends Component{
+  render(){
+    return(
+      <div>
+      <p className={MyApp}>{2+2}</p>
+      </div>
+    );
+  }
+}
+export default MyApp;
