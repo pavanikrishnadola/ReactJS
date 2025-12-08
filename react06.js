deafbacbb33d2e9bae72d370dba75ec431becdc2
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2025-12-08 14:26:57 +0530

    Create react06.js

---
+++ react06.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <h1 className="hello">Hello React</h1>
+        {/*This is component in JSX*/}
+      </div>
+    );
+  }
+}
+export default App;
