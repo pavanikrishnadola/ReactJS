ab7d25ef82257782eee2a1ec05016200a7b18bb8
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2025-12-08 14:13:33 +0530

    Create react03.js

---
+++ react03.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+class MyApp extends Component{
+  reender(){
+    return(
+      <div>
+      <p className="myCalci">{2+2}</p>
+      </div>
+    );
+  }
+}
+export default MyApp;
