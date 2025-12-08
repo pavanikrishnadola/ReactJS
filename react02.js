fa518b4d4aab74ee76e17d7a73a32497fba06e6e
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2025-12-08 14:09:44 +0530

    Create react02.js

---
+++ react02.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+class App extends Component{
+  render(){
+    return(
+      <div>
+      <h1>Pavnai Krishna</h1>
+      <h2>Pavnai Krishna</h2>
+      <p>This is reactjs code</p>
+      </div>
+    );
+  }
+}
+export default App;
