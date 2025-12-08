bef6ba9803f6fe14597432ce3e4c5fce752ddd86
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2025-12-08 14:46:38 +0530

    Create react07.js

---
+++ react07.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+class App extends Component{
+  render(){
+    var myStyle={fontSize:80,fontFamily:'carier',color:'#003300'}
+    return(
+      <div>
+        <h1 style={myStyle}>Pavani Krishna</h1>
+      </div>
+    );
+  }
+}
+export default App;
