d04ec14bbe97df379020364597afff64c32eee97
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2025-12-11 09:48:55 +0530

    Create react16.js

---
+++ react16.js
@@ -0,0 +1,20 @@
+import React,{Component} from "react";
+class App extends React.Component{
+  constructor(){
+    super();
+    this.forceUpdateState=this.forceUpdateState.bind(this);
+  }
+  forceUpdateState(){
+    this.forceUpdate();
+  };
+  render(){
+    return(
+      <div>
+      <h1>Example to generate random number</h1>
+      <h3>Random Number:{Math.random()}</h3>
+      <button onClick={this.forceUpdateState}>ForceUpdate</button>
+      </div>
+    );
+  }
+}
+export default App;
