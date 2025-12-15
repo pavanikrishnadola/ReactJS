393206e7c7d49a5223d609a1a18afa7da48561d3
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2025-12-15 10:07:03 +0530

    Update react20.js

--- react20.js
+++ react20.js
@@ -1,1 +1,25 @@
-
+import React,{Component} from "react";
+class ExampleComponent extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      message:"Hello,World!",
+      count:0
+    };
+  }
+  incrementCount =() => {
+    this.setState((prevState)=>({
+      count:prevState.count+1
+    }));
+  }
+  render(){
+    return(
+      <div>
+        <h1>{this.state.message}</h1>
+        <p>Count:{this.state.count}</p>
+        <button onClick={this.incrementCount}>Increment</button>
+      </div>
+    );
+  }
+}
+export default ExampleComponent;
