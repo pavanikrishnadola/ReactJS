e3a4a5fb12d1ed4bff1e2fd22ed0d694a7f9f63a
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2025-12-29 12:18:21 +0530

    Create App.js

---
+++ react41/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NameList from "./NameList";
+const App = () =>{
+  return(
+    <div>
+      <h1>Name List</h1>
+      <NameList/>
+    </div>
+  );
+};
+export default App;
