2d7bcd473fedf0b52c8e5f50e8dd91ac54ea0af0
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2025-12-29 11:56:10 +0530

    Create App.js

---
+++ react42/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ItemList from "./ItemList";
+const App = () =>{
+  return(
+    <div>
+      <h1>Item List</h1>
+      <ItemList/>
+    </div>
+  );
+};
+export default App;
