0db393bddf1fa4ab49e3e82dfb80fc24aae74015
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2025-12-29 12:34:59 +0530

    Create react39.js

---
+++ react39.js
@@ -0,0 +1,20 @@
+import React,{useState} from "react";
+function App(){
+  const [isLoggedIn,setIsLoggedIn]=useState(false);
+  const handleLogin =() =>{
+    setIsLoggedIn(true);
+  };
+  const handleLogout = () =>{
+    setIsLoggedIn(false);
+  };
+  return(
+    <div>
+      {isLoggedIn ? (
+        <button onClick={handleLogout}>Logout</button>
+      ):(
+        <button onClick={handleLogin}>Login</button>
+      )}
+    </div>
+  );
+};
+export default App;
