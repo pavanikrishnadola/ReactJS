6ef52bfd566119077c2522a58324380f512a2014
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2025-12-30 11:35:48 +0530

    Update react37.js

--- react37.js
+++ react37.js
@@ -9,5 +9,41 @@ class LoginControl extends React.Component{
   handleLoginClick(){
     this.setState({isLoggedIn: true});
   }
-  
+  handleLogoutClick(){
+    this.setState({isLoggedIn:false});
+  }
+  render(){
+    const isLoggedIn=this.state.isLoggedIn;
+    let button;
+    if(isLoggedIn){
+      button=<LogoutButton onClick={this.handleLogoutClick}/>
+    }
+    else{
+      button=<LoginButton onClick={this.handleLoginClick}/>
+    }
+    return(
+      <div>
+        <Greeting isLoggedIn={isLoggedIn}/>
+        {button}
+      </div>
+    );
+  }
 }
+function Greeting(props){
+  const isLoggedIn=props.isLoggedIn;
+  if(isLoggedIn){
+    return<h1>Welcome back</h1>
+  }
+  return<h1>Please Sign Up</h1>
+}
+function LoginButton(props){
+  return(
+    <button onClick={props.onClick}>Login</button>
+  );
+}
+function LogoutButton(props){
+  return(
+    <button onClick={props.onClick}>Logout</button>
+  );
+}
+export default LoginControl;
