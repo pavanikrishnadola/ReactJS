dda495554ef7a2fc73ecd89c2882ac27f631c21d
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2025-12-30 11:18:21 +0530

    Create react50.js

---
+++ react50.js
@@ -0,0 +1,12 @@
+import React from "react";
+class App extends React.Component{
+  render(){
+    return(
+      <React.Fragment>
+        <div>Hello,World!</div>
+        <p>This is example for Fragment</p>
+      </React.Fragment>
+    );
+  }
+}
+export default App;
