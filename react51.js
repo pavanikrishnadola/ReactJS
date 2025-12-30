eff33213579d6bc9af3070ca70fce03fb2858355
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2025-12-30 11:21:40 +0530

    Create react51.js

---
+++ react51.js
@@ -0,0 +1,12 @@
+import React from "react";
+class Columns extends React.Component{
+  render(){
+    return(
+      <>
+      <h2>Hello,World!</h2>
+      <p>Welcome to JAVA</p>
+      </>
+    );
+  }
+}
+export default Columns;
