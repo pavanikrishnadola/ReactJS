e251b159f4b7d7ab9d23560c94391d8e2334a28f
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2025-12-30 11:10:56 +0530

    Update react49.js

--- react49.js
+++ react49.js
@@ -1,1 +1,22 @@
-
+import React,{useCallback} from "react";
+const ScrollToElement = () =>{
+  let myRef=null;
+  const setRef=useCallback(element => {
+    if(element){
+      myRef=element;
+    }
+  },[]);
+  const handleClick = () => {
+    if(myRef){
+      myRef.scrollIntoView({behavior:'smooth'});
+    }
+  };
+  return(
+    <div>
+      <button onClick={handleClick}>Scroll To Element</button>
+      <div style={{height:'1500px'}}></div>
+      <div ref={setRef} style={{height:'100px',background:'yellow'}}>Scroll To This Element</div>
+    </div>
+  )
+}
+export default ScrollToElement;
