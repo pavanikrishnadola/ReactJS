23487e350665378704d2f7c44ef864750ca46d52
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2026-01-06 10:20:49 +0530

    Create react56.js

---
+++ react56.js
@@ -0,0 +1,10 @@
+import React from "react";
+function App(){
+  return(
+    <div>
+      <h1 style={{backgroundColor:"red"}}>CSS APPLIED</h1>
+      <p>CSS Applied</p>
+    </div>
+  );
+}
+export default App;
