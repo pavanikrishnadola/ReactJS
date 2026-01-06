505f50565801c7a4cb194463d77adcc332dd7cb5
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2026-01-06 10:24:35 +0530

    Create react58.js

---
+++ react58.js
@@ -0,0 +1,10 @@
+import React from "react";
+function App(){
+  return(
+    <div>
+      <h1 style={{color:"Red"}}>Hello!</h1>
+      <p style={{backgroundColor:"lightgreen"}}>Welcome You All.</p>
+    </div>
+  );
+}
+export default App;
