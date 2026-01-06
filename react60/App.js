ddf35821d88ba21b370ca642cb9eb4d43c520c40
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2026-01-06 10:33:29 +0530

    Create App.js

---
+++ react60/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css';
+class App extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Hello!</h1>
+        <p>Welcome.</p>
+      </div>
+    );
+  }
+}
+export default App;
