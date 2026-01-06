91a983eb4b7d808623a3db439628afb0b4eafc5f
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2026-01-06 11:21:53 +0530

    Create react30.js

---
+++ react30.js
@@ -0,0 +1,11 @@
+import React from "react";
+function App(){
+  return(
+    <form>
+      <h3>Sign-up Form</h3>
+      <label>Name:</label><input type="text" placeholder="Enter User Name"></input><br></br>
+      <label>Password:</label><input type="password" placeholder="Enter Password"></input>
+    </form>
+  );
+}
+export default App;
