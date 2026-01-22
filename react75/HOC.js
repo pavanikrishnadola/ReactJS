5c734416385965d04e2b9be1416ac454cb20d744
Pavani Krishna Dola <232014247+pavanikrishnadola@users.noreply.github.com>
2026-01-22 11:37:53 +0530

    Add Higher-Order Component (HOC) implementation

---
+++ react75/HOC.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+export default function Hoc (HocComponent){
+    return class extends Component{
+        render(){
+            return(
+                <div>
+                    <HocComponent></HocComponent>
+                </div>
+            )
+        }
+    }
+}
